4207c9a770dcf2cf1d4a73d1e24f40ff3dc6eea8
outragedline <outragedline@proton.me>
2023-11-27 20:17:46 -0300

    Update: valor minimo de um dado agora é 1 ao inves de 0

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,6 @@ from random import randint
 def rolar(quantidade, lados):
     resultados = []
     for i in range(quantidade):
-        resultado = randint(0, lados)
+        resultado = randint(1, lados)
         resultados.append(resultado)
     return resultados
