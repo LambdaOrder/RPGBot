8613f02bed9299386cfcfe641e0a57ebada89df3
outragedline <outragedline@proton.me>
2023-12-13 03:19:05 -0300

    utils.py foi movido para a pasta services

--- src/services/utils.py
+++ src/services/utils.py
@@ -1,7 +1,9 @@
 from random import randint
+import math
 def rolar(quantidade, lados):
     resultados = []
     for i in range(quantidade):
         resultado = randint(1, lados)
         resultados.append(resultado)
     return resultados
+
