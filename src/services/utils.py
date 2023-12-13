5a887df0b72803b073e33beb1248ad6cfcd925b1
outragedline <outragedline@proton.me>
2023-12-13 03:20:52 -0300

    Adcionada função para calculo de proficiencia

--- src/services/utils.py
+++ src/services/utils.py
@@ -7,3 +7,17 @@ def rolar(quantidade, lados):
         resultados.append(resultado)
     return resultados
 
+def proficiencia(atributo, bonus):
+    """
+    Esta função é utilizada para calcular prroficiencia em pericias e testes de resistencia
+
+    atributo: nivel do atributo correspondente
+    bonus: bonus de proficiencia do personagem
+
+
+    >>> proficiencia(1,2)
+    -3
+    """
+    pfc = math.floor((atributo - 10)/2)
+    pfc += bonus
+    return pfc
